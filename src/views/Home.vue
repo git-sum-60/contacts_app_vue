9dc881c151f9414438e62a3e86c726e1bf6ce296
brian <riceb53@gmail.com>
2020-04-12 11:55:05 -0400

    new/create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    first name: <input type="text" v-model="newContact.firstName">
+    last name: <input type="text" v-model="newContact.lastName">
+    email: <input type="text" v-model="newContact.email">
+    phoneNumber: <input type="text" v-model="newContact.phoneNumber">
+    address: <input type="text" v-model="newContact.address">
+
+    <button v-on:click="addContact()">Add new contact</button>
+    
     <div v-bind:key="contact.id" v-for="contact in contacts">
       <p>first_name: {{ contact.first_name }}</p>
       <p>last_name: {{ contact.last_name }}</p>
@@ -22,7 +30,13 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      contacts: []
+      contacts: [],
+      newContact: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: ""        
+      }
     };
   },
   created: function() {
@@ -32,6 +46,23 @@ export default {
       this.contacts = response.data;
     })
   },
-  methods: {}
+  methods: {
+    addContact: function() {
+      console.log('i am adding a contact...')
+      console.log(this.newContact.firstName)
+
+      var params = {
+        first_name: this.newContact.firstName,
+        last_name: this.newContact.lastName,
+        email: this.newContact.email,
+        phone_number: this.newContact.phoneNumber,
+        address: this.newContact.address
+      }
+
+      axios.post('/api/contacts', params).then(response => {
+        console.log(response.data);
+      })
+    }
+  }
 };
 </script>
